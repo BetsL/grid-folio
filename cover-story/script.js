a80a157ba70112f971f49433f23b3d86baa559d9
Bets <elizabeth.lidstrom@gmail.com>
2019-07-11 21:06:21 -0400

    cover complete minus media query tweaks and final story steps

--- cover-story/script.js
+++ cover-story/script.js
@@ -1,10 +1,22 @@
-const slider = document.getElementById('myRange');
-const output = document.getElementById('demo');
 
-// display default slider value
-output.innerHTML = slider.value;
+/* ---------- SLIDER + STORY ---------- */
 
-// update the current slider value
-slider.oninput = function(){
-	output.innerHTML = this.value;
-} 
+// values to step to 
+const values = ['Once there was a teacher.', 
+			  'She liked to make things.', 
+			  'She taught her students to make things', 
+			  'Projects filled every corner of her classroom',
+			  'red',
+			  'orange',
+			  'yellow',
+			  'green',
+			  'blue',
+			  'purple'];
+
+const input = document.getElementById('myStory'),
+   output = document.getElementById('output');
+
+input.oninput = function(){
+	output.innerHTML = values[this.value];
+};
+input.oninput(); // set default value 

9a4e330f7b232af6d6c2c5ba2dde781996e8b54f
elizabethharkins <elizabeth.lidstrom@gmail.com>
2021-01-28 17:19:42 -0500

    mobile nav cubic-bezier fun, minor edits

--- js/cover-pg/utils.js
+++ js/cover-pg/utils.js
@@ -1,14 +1,16 @@
 
 
-import { mainNav } from "./elements.js";
+import { mainNav, navBarToggle } from "./elements.js";
 
 export function toggleMainNav() {
+	this.classList.toggle("active");
 	mainNav.classList.toggle("active");
 	mainNav.querySelectorAll(".main-nav li").forEach(li => li.classList.add("active"));
 }
 
 export function closeMainNav() {
 	mainNav.classList.remove("active");
+	navBarToggle.classList.remove("active");
 	mainNav.querySelectorAll(".main-nav li").forEach(li => li.classList.remove("active"));
 }
 
